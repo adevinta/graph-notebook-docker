16d2ff6be9e5727a8befd7816330071de6e20e95
Manel Montilla <manel.montilla@gmail.com>
2021-08-03 16:06:49 +0200

    ci: add a script to push docker images to a registry

---
+++ script/push_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Copyright 2021 Adevinta
+
+# This script tags and pushes a locally existent docker image to a specified new
+# tag. It receives two parameters, the first one specifies the tag of the local
+# image, and the second one the tag to push the image to. It expects the env
+# variables: DOCKER_USERNAME and DOCKER_PASSWORD to be set to the credentials of
+# a valid user with permission to push to the docker imaghe registry used in the
+# tag the script will push the image to.
+set -ev
+if [ -z "$1" ]
+  then
+    echo "no local image tag specified"
+fi
+
+if [ -z "$2" ]
+  then
+    echo "no image tag to push to specified"
+fi
+
+LOCAL_TAG=$1
+PUSH_TAG=$2
+docker tag $LOCAL_TAG $PUSH_TAG
+echo ${DOCKER_PASSWORD} | docker login -u $DOCKER_USERNAME --password-stdin
+echo "pushing image to repository ${PUSH_TAG}"
+docker push $PUSH_TAG
+docker logout
